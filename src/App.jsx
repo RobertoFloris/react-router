14bed04c40a813ebea8493786bb1820f1cc55b8b
RobertoFloris <florisroberto99@gmail.com>
2025-01-23 17:56:44 +0100

    [fix] structure [add] LayoutDefault.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import HomePage from "./components/HomePage";
-import AboutPage from "./components/AboutPage";
-import ListPost from "./components/ListPost";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ListPost from "./pages/ListPost";
+
+import LayoutDefault from "./layouts/layoutDefault";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={LayoutDefault}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+          <Route path="/list" Component={ListPost} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
