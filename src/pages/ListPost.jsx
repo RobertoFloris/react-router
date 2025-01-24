7022425166188a50abb58df4ddd774b9b63de6b8
RobertoFloris <florisroberto99@gmail.com>
2025-01-24 12:05:06 +0100

    [add] list post

--- src/pages/ListPost.jsx
+++ src/pages/ListPost.jsx
@@ -1,6 +1,44 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 const ListPost = () => {
+
+  const apiUrl = "http://localhost:3000"
+
+  const [posts, setPosts] = useState([])
+
+
+  const fetchPosts = () => {
+    axios.get(`${apiUrl}/posts`)
+      .then(res => {
+        console.log(res.data)
+        setPosts(res.data)
+      })
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
+
   return (
-    <div>ListPost</div>
+    <div className="container">
+      <ul>
+        {posts?.map(post => (
+          <li key={post.id} className="d-flex justify-content-between m-3">
+            <span>
+              <a>{post.title}</a>
+            </span>
+            <span>
+              <button className="btn btn-danger">
+                Elimina
+              </button>
+            </span>
+
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
